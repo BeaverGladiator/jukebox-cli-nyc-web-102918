bf4a6d2224ca2ae5059306dab684ac3b44bdb48a
Matthew Migliore <mmigliore311@gmail.com>
2018-10-19 13:40:45 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,3 +18,13 @@ songs = [
 # users_name = gets.chomp
 
 # puts say_hello(users_name)
+
+puts "Please enter a command:"
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end 
